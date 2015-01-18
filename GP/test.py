2581707e73a0c36c8ec796f884e0f5d58ed1ab6b
Amir Dezfouli <AT@Amirs-MacBook-Pro.local>
2015-01-18 21:06:13 +1100

    BCM and direct_gp added

--- GP/test.py
+++ GP/test.py
@@ -1,20 +1,34 @@
 from matplotlib.pyplot import show
-from GPy.examples import tutorials
-from GPy.examples.regression import olympic_marathon_men
-
 import numpy as np
+from BCM import BCM_GP
 import GPy
+import direct_gp
 
-def test():
-    X = np.random.uniform(-3.,3.,(20,1))
-    Y = np.sin(X) + np.random.randn(20,1)*0.05
 
-    kernel = GPy.kern.rbf(input_dim=1, variance= 4., lengthscale=3.)
+def generate_samples():
+    seed=1000
+    num_samples=500
+    noise=0.02
+    np.random.seed(seed=seed)
+    num_in = 1
+    X = np.random.uniform(low=-1.0, high=1.0, size=(num_samples, num_in))
+    X.sort(axis=0)
+    rbf = GPy.kern.rbf(num_in, variance=1., lengthscale=np.array((0.25,)))
+    white = GPy.kern.white(num_in, variance=noise)
+    kernel = rbf + white
+    K = kernel.K(X)
+    y = np.reshape(np.random.multivariate_normal(np.zeros(num_samples), K), (num_samples, 1))
+    return X, y, kernel, noise
 
-    m = GPy.models.GPRegression(X, Y, kernel)
 
-    print m
-    m.plot()
+def test_direct_gp(optimize=True, plot=True):
+    X, y, rbf, noise = generate_samples()
+    # D = np.zeros((len(X), 2))
+    # D[:, 1] = y[:, 0]
+    # D[:, 0] = X[:, 0]
+    # print D[:, [0]]
+    m = BCM_GP(X, GPy.likelihoods.Gaussian(y, False), rbf, False, noise, 1)
+    m.plot(plot_raw=True)
     show(block=True)
 
-test()
+test_direct_gp()
