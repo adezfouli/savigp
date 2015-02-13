0743c98b0e27cbd4f4810ff56745aa45e302e74e
adezfouli <akdezfuli@gmail.com>
2015-02-14 02:01:03 +1100

    removed GPY and also made some modification to make the algorithm numerically stable

--- GPy/GPy/examples/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2012, GPy authors (see AUTHORS.txt).
-# Licensed under the BSD 3-clause license (see LICENSE.txt)
-
-import classification
-import regression
-import dimensionality_reduction
-import tutorials
-import stochastic
