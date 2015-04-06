04b43ae16ffa1f60e6049197a678325cd48ec4f8
adezfouli <akdezfuli@gmail.com>
2015-04-06 22:18:18 +1000

    increased sample size and max iter

--- GP/experiments.py
+++ GP/experiments.py
@@ -87,20 +87,20 @@ class Experiments:
 
         if method == 'full':
             m = GSAVIGP_SignleComponenet(Xtrain, Ytrain, Xtrain.shape[0], UnivariateGaussian(np.array(gaussian_sigma)),
-                                 kernel, 10000, None)
-            Optimizer.optimize_model(m, 10000, True, ['mog', 'hyp', 'll'])
+                                 kernel, 100000, None)
+            Optimizer.optimize_model(m, 100000, True, ['mog', 'hyp', 'll'])
             y_pred, var_pred = m.predict(Xtest)
 
         if method == 'mix1':
             m = GSAVIGP_Diag(Xtrain, Ytrain, Xtrain.shape[0], 1, UnivariateGaussian(np.array(gaussian_sigma)),
-                                 kernel, 10000, None)
-            Optimizer.optimize_model(m, 10000, True, ['mog', 'hyp', 'll'])
+                                 kernel, 100000, None)
+            Optimizer.optimize_model(m, 100000, True, ['mog', 'hyp', 'll'])
             y_pred, var_pred = m.predict(Xtest)
 
         if method == 'mix2':
             m = GSAVIGP_Diag(Xtrain, Ytrain, Xtrain.shape[0], 2, UnivariateGaussian(np.array(gaussian_sigma)),
-                                 kernel, 10000, None)
-            Optimizer.optimize_model(m, 10000, True, ['mog', 'hyp', 'll'])
+                                 kernel, 100000, None)
+            Optimizer.optimize_model(m, 100000, True, ['mog', 'hyp', 'll'])
             y_pred, var_pred = m.predict(Xtest)
 
         if method == 'gp':
