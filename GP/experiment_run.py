06ba72d7ee454340d51460d3b43ff4497a6dfbdf
adezfouli <akdezfuli@gmail.com>
2015-04-14 16:57:40 +1000

    changed multithread to multi process

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -1,21 +1,23 @@
 from experiments import Experiments
 from plot_results import PlotOutput
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import ThreadPool, Pool
 
 
 class ExperimentRunner:
 
     @staticmethod
-    def parallel_experiment(f, n_thread):
+    def get_configs():
         configs = []
+        expr_names = [Experiments.boston_data.__name__]
+        expr_names = [Experiments.wisconsin_breast_cancer_data.__name__]
         methods = ['mix1', 'mix2', 'full']
         sparse_factor = [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]
-        for m in methods:
-            for s in sparse_factor:
-                configs.append({'method': m, 'sparse_factor': s})
+        for e in expr_names:
+            for m in methods:
+                for s in sparse_factor:
+                    configs.append({'method': m, 'sparse_factor': s, 'method_to_run': e})
 
-        p = ThreadPool(n_thread)
-        p.map(f, configs)
+        return configs
 
     @staticmethod
     def boston_experiment():
@@ -38,9 +40,12 @@ class ExperimentRunner:
         # PlotOutput.plot_output_all('boston', Experiments.get_output_path(),
         #                        lambda x: x['c'] == '1' and (x['m'] in ['mix2', 'mix1', 'full', 'gp']), False)
 
+def run_config(config):
+    getattr(Experiments, config['method_to_run'])(config)
+
 if __name__ == '__main__':
-    plots = []
-    ExperimentRunner.parallel_experiment(Experiments.wisconsin_breast_cancer_data, 2)
-    ExperimentRunner.parallel_experiment(Experiments.boston_data, 2)
-    ExperimentRunner.boston_experiment()
-    ExperimentRunner.wisconsin_breast_experiment()
+    n_process = 4
+    p = Pool(n_process)
+    p.map(run_config, ExperimentRunner.get_configs())
+    # ExperimentRunner.boston_experiment()
+    # ExperimentRunner.wisconsin_breast_experiment()
