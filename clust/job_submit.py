5a099fa0306bcb376d8b8e7eedf24329e1f963b5
adezfouli <akdezfuli@gmail.com>
2015-04-28 11:21:31 +1000

    automatic job submission

--- clust/job_submit.py
+++ clust/job_submit.py
@@ -4,36 +4,41 @@ from popen2 import popen2
 import time
 
 #many = int(sys.argv[1])
+from subprocess import Popen, PIPE, STDOUT
+
 many=1
 for i in range(0, many):
 
     # Open a pipe to the qsub command.
-    output, input = popen2('qsub')
 
+    name = 'wisc'
     # Customize your options here
-    job_name = "adezfouli2%s_%s_test" % (str(i).zfill(3), '')
+    job_name = "adez%s_%s" % (str(i).zfill(3), name)
     walltime = "4:00:00"
-    #processors = "nodes=1:ppn=1:scalemp" #can put avx2 here as well to use new computers
-    processors = "nodes=1:ppn=1" #Currently avx2 is not working
+    processors = "nodes=1:ppn=64"
     command = "./run_job.sh %s" % (str(i))
-    #command = "StartConda;python myfile.py %s" % (str(i))
     job_string = """#!/bin/bash
     #PBS -N %s
     #PBS -l walltime=%s
     #PBS -l %s
+    #PBS -q short64
     #PBS -o /home/z3510738/code/output/%s.out
     #PBS -e /home/z3510738/code/error/%s.err
-    #PBS -M a.dezfouli@unsw.edu.au@unsw.edu.au
+    #PBS -M a.dezfouli@unsw.edu.au
+    #PBS -m unsw_job_finished
     cd $PBS_O_WORKDIR
+    cd /home/z3510738/code/savigp/clust/
+    chmod +x ./run_job.sh
     %s""" % (job_name, walltime, processors, job_name, job_name, command)
     #"\&".join([command_N"\n"]*N) amd change ppn to N
     # Send job_string to qsub
-    input.write(job_string)
-    input.close()
-
-    # Print your job and the response to the screen
+    out = open('auto_run.pbs', 'w')
+    out.write(job_string)
+    out.close()
+    p = Popen(['qsub', 'auto_run.pbs'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    output = p.communicate()[0]
+    print output
     print job_string
-    print output.read()
+    time.sleep(2.0)
 
-    time.sleep(0.1)
 
