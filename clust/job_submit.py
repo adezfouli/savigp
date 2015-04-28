030dda61c68d546929aa00acac8999d3512eb050
adezfouli <akdezfuli@gmail.com>
2015-04-28 11:40:23 +1000

    idented file

--- clust/job_submit.py
+++ clust/job_submit.py
@@ -11,25 +11,25 @@ for i in range(0, many):
 
     # Open a pipe to the qsub command.
 
-    name = 'wisc'
+    name = 'boston'
     # Customize your options here
     job_name = "adez%s_%s" % (str(i).zfill(3), name)
     walltime = "4:00:00"
     processors = "nodes=1:ppn=64"
     command = "./run_job.sh %s" % (str(i))
     job_string = """#!/bin/bash
-    #PBS -N %s
-    #PBS -l walltime=%s
-    #PBS -l %s
-    #PBS -q short64
-    #PBS -o /home/z3510738/code/output/%s.out
-    #PBS -e /home/z3510738/code/error/%s.err
-    #PBS -M a.dezfouli@unsw.edu.au
-    #PBS -m unsw_job_finished
-    cd $PBS_O_WORKDIR
-    cd /home/z3510738/code/savigp/clust/
-    chmod +x ./run_job.sh
-    %s""" % (job_name, walltime, processors, job_name, job_name, command)
+#PBS -N %s
+#PBS -l walltime=%s
+#PBS -l %s
+#PBS -q short64
+#PBS -o /home/z3510738/code/output/%s.out
+#PBS -e /home/z3510738/code/error/%s.err
+#PBS -M a.dezfouli@unsw.edu.au
+#PBS -m unsw_job_finished
+cd $PBS_O_WORKDIR
+cd /home/z3510738/code/savigp/clust/
+chmod +x ./run_job.sh
+%s""" % (job_name, walltime, processors, job_name, job_name, command)
     #"\&".join([command_N"\n"]*N) amd change ppn to N
     # Send job_string to qsub
     out = open('auto_run.pbs', 'w')
