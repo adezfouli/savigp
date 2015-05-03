809a5bd4c6ed0ca70a9e3112f44a1b48bfccbf7b
adezfouli <akdezfuli@gmail.com>
2015-05-03 19:22:22 +1000

    added hyp to USPS

--- GP/experiments.py
+++ GP/experiments.py
@@ -272,7 +272,7 @@ class Experiments:
         cond_ll = SoftmaxLL(3)
 
         names.append(Experiments.run_model(Xtest, Xtrain, Ytest, Ytrain, cond_ll, kernel, method, name, d['id'], num_inducing,
-                                 num_samples, sparsify_factor, ['mog'], IdentityTransformation, True,
+                                 num_samples, sparsify_factor, ['mog', 'hyp'], IdentityTransformation, True,
                                  config['log_level']))
 
     @staticmethod
