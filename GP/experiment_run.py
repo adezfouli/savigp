cdca5c2bc7b74b8b072e8c858236ee898c0bc3c6
adezfouli <akdezfuli@gmail.com>
2015-05-12 10:29:40 +1000

    t

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -46,7 +46,7 @@ class ExperimentRunner:
 
     @staticmethod
     def boston_experiment():
-        Experiments.boston_data({'method': 'mix2', 'sparse_factor': 0.8, 'run_id': 3, 'log_level': logging.DEBUG})
+        Experiments.boston_data({'method': 'full', 'sparse_factor': 0.8, 'run_id': 3, 'log_level': logging.DEBUG})
 
     @staticmethod
     def wisconsin_breast_experiment():
@@ -96,11 +96,11 @@ def run_config_serial(config):
 
 if __name__ == '__main__':
     logger = Experiments.get_logger('general_' + Experiments.get_ID(), logging.DEBUG)
-    n_process = 30
-    p = Pool(n_process)
-    p.map(run_config, ExperimentRunner.get_configs())
+    # n_process = 30
+    # p = Pool(n_process)
+    # p.map(run_config, ExperimentRunner.get_configs())
     # run_config_serial(ExperimentRunner.get_configs())
-    # ExperimentRunner.boston_experiment()
+    ExperimentRunner.boston_experiment()
     # ExperimentRunner.wisconsin_breast_experiment()
     # ExperimentRunner.USPS_experiment()
     # ExperimentRunner.mining_experiment()
