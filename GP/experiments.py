89987d381d115ee50f9932bf1aaaba74880c3b7c
adezfouli <akdezfuli@gmail.com>
2015-05-12 13:11:57 +1000

    increased latent noise

--- GP/experiments.py
+++ GP/experiments.py
@@ -138,7 +138,7 @@ class Experiments:
         opt_max_fun_evals = None
         opt_per_iter = 40
         max_iter = 200
-        latent_noise = 0.002
+        latent_noise = 0.005
         xtol = 1e-3
         total_time = None
         timer_per_iter = None
