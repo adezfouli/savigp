20a6373a41836cdc57d1b5d02b6c5edbe57e9dc8
adezfouli <akdezfuli@gmail.com>
2015-05-12 11:22:48 +1000

    boston

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -96,11 +96,11 @@ def run_config_serial(config):
 
 if __name__ == '__main__':
     logger = Experiments.get_logger('general_' + Experiments.get_ID(), logging.DEBUG)
-    # n_process = 30
-    # p = Pool(n_process)
-    # p.map(run_config, ExperimentRunner.get_configs())
+    n_process = 30
+    p = Pool(n_process)
+    p.map(run_config, ExperimentRunner.get_configs())
     # run_config_serial(ExperimentRunner.get_configs())
-    ExperimentRunner.boston_experiment()
+    # ExperimentRunner.boston_experiment()
     # ExperimentRunner.wisconsin_breast_experiment()
     # ExperimentRunner.USPS_experiment()
     # ExperimentRunner.mining_experiment()
