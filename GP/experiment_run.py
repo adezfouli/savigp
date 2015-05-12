0aa64c3f5b6ad3ec383351eb773596b1ae003b3a
adezfouli <akdezfuli@gmail.com>
2015-05-12 12:25:00 +1000

    boston

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -28,8 +28,8 @@ class ExperimentRunner:
 
     @staticmethod
     def get_experiments():
-        # return [Experiments.boston_data.__name__]
-        return [Experiments.wisconsin_breast_cancer_data.__name__]
+        return [Experiments.boston_data.__name__]
+        # return [Experiments.wisconsin_breast_cancer_data.__name__]
         # return [Experiments.USPS_data.__name__]
         # return [Experiments.creep_data.__name__]
         # return [Experiments.abalone_data.__name__]
@@ -96,11 +96,11 @@ def run_config_serial(config):
 
 if __name__ == '__main__':
     logger = Experiments.get_logger('general_' + Experiments.get_ID(), logging.DEBUG)
-    # n_process = 30
-    # p = Pool(n_process)
-    # p.map(run_config, ExperimentRunner.get_configs())
+    n_process = 30
+    p = Pool(n_process)
+    p.map(run_config, ExperimentRunner.get_configs())
     # run_config_serial(ExperimentRunner.get_configs())
-    ExperimentRunner.boston_experiment()
+    # ExperimentRunner.boston_experiment()
     # ExperimentRunner.wisconsin_breast_experiment()
     # ExperimentRunner.USPS_experiment()
     # ExperimentRunner.mining_experiment()
