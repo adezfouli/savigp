24b8175457ccde03fb87c0b23adf35b8be2bade4
adezfouli <akdezfuli@gmail.com>
2015-05-13 11:10:56 +1000

    corrected random clust assignment

--- GP/savigp_test.py
+++ GP/savigp_test.py
@@ -19,7 +19,7 @@ from GPy.util.linalg import mdot
 import numpy as np
 from optimizer import *
 from savigp import Configuration
-from likelihood import MultivariateGaussian, UnivariateGaussian
+from likelihood import UnivariateGaussian
 from grad_checker import GradChecker
 from plot import plot_fit
 from util import chol_grad, jitchol, bcolors
@@ -35,11 +35,11 @@ class SAVIGP_Test:
         num_process = -1
         ll = None
         gaussian_sigma = None
-        if likelihood == 'multi_Gaussian':
-            gaussian_sigma = 0.5
-            num_process = 3
-            cov = np.eye(num_process) * gaussian_sigma
-            ll = MultivariateGaussian(np.array(cov))
+        # if likelihood == 'multi_Gaussian':
+        #     gaussian_sigma = 0.5
+        #     num_process = 3
+        #     cov = np.eye(num_process) * gaussian_sigma
+        #     ll = MultivariateGaussian(np.array(cov))
         if likelihood == 'univariate_Gaussian':
             gaussian_sigma = 0.5
             num_process = 1
