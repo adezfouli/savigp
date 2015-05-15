7027e7f931f849ec20d96312bb52f285948c37b8
adezfouli <akdezfuli@gmail.com>
2015-05-16 00:31:51 +1000

    added mnist data

--- GP/data_source.py
+++ GP/data_source.py
@@ -1,4 +1,7 @@
+import gzip
+import os
 from GPy.util import datasets
+import cPickle
 import pandas
 from pandas.util.testing import DataFrame
 from ExtRBF import ExtRBF
@@ -162,6 +165,71 @@ class DataSource:
             })
 
         return data
+
+    @staticmethod
+    def mnist_data():
+        ''' Loads the dataset
+
+        :type dataset: string
+        :param dataset: the path to the dataset (here MNIST)
+        '''
+
+        #############
+        # LOAD DATA #
+        #############
+
+        dataset = 'mnist.pkl.gz'
+
+        # Download the MNIST dataset if it is not present
+        data_dir, data_file = os.path.split(dataset)
+        if data_dir == "" and not os.path.isfile(dataset):
+            # Check if dataset is in the data directory.
+            new_path = os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "data",
+                dataset
+            )
+            if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
+                dataset = new_path
+
+        if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
+            import urllib
+            origin = (
+                'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+            )
+            print 'Downloading data from %s' % origin
+            urllib.urlretrieve(origin, dataset)
+
+        print '... loading data'
+
+        # Load the dataset
+        f = gzip.open(dataset, 'rb')
+        train_set, valid_set, test_set = cPickle.load(f)
+        f.close()
+
+        test_Y = np.zeros((test_set[1].shape[0], 10))
+        test_Y[np.arange(test_set[1].shape[0]), test_set[1]] = 1
+        train_Y = np.zeros((train_set[1].shape[0], 10))
+        train_Y[np.arange(train_set[1].shape[0]), train_set[1]] = 1
+
+        data = []
+        data.append({
+                'train_Y': train_Y,
+                'train_X': train_set[0],
+                'test_Y': test_Y,
+                'test_X': train_set[0],
+                'id': 0
+            })
+
+        return data
+        #train_set, valid_set, test_set format: tuple(input, target)
+        #input is an numpy.ndarray of 2 dimensions (a matrix)
+        #witch row's correspond to an example. target is a
+        #numpy.ndarray of 1 dimensions (vector)) that have the same length as
+        #the number of rows in the input. It should give the target
+        #target to the example with the same index in the input.
+
 
 if __name__ == '__main__':
     X, Y = DataSource.wisconsin_breast_cancer_data()
