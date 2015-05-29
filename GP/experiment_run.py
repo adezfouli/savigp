7cbf1c5dfe08f6573d1983548607c96796498914
adezfouli <akdezfuli@gmail.com>
2015-05-29 15:57:12 +1000

    mnist 0.04

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -105,12 +105,12 @@ class ExperimentRunner:
     @staticmethod
     def mnist_experiment():
         Experiments.MNIST_data({'method': 'full',
-                                'sparse_factor': 0.02,
+                                'sparse_factor': 0.04,
                                 'run_id': 1,
                                 'log_level': logging.DEBUG,
                                 'n_thread': 10,
-                                'partition_size': 3000,
-                                # 'image': '../results/mnist_2/'
+                                'partition_size': 2000,
+                                'image': '../results/mnist_2/'
                                 })
 
     @staticmethod
